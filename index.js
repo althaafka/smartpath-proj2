57501300ba155aa0a7181b41e1844bd9e479f645
althaafka <althaafka004@gmail.com>
2024-07-26 14:20:39 +0700

    feat: darkmode

--- index.js
+++ index.js
@@ -28,3 +28,44 @@ document.addEventListener('DOMContentLoaded', function() {
         skillHtml.insertAdjacentHTML('beforeend', skillElement);
     })
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        enableDarkMode();
+    }
+
+    darkModeToggle.addEventListener('click', function () {
+        if (document.body.classList.contains('dark-mode')) {
+            disableDarkMode();
+        } else {
+            enableDarkMode();
+        }
+    });
+
+    function enableDarkMode() {
+        document.body.classList.add('dark-mode');
+        document.querySelectorAll('.card').forEach(card => card.classList.add('dark-mode'));
+        document.querySelectorAll('.btn').forEach(btn => btn.classList.add('dark-mode'));
+        document.querySelectorAll('.form-control').forEach(input => input.classList.add('dark-mode'));
+        document.querySelectorAll('.carousel-item').forEach(item => item.classList.add('dark-mode'));
+        document.querySelector('footer').classList.add('dark-mode');
+        // document.querySelector('skill').style.color = 'black';
+        localStorage.setItem('dark-mode', 'enabled');
+    }
+
+    function disableDarkMode() {
+        document.body.classList.remove('dark-mode');
+        document.querySelector('.navbar').classList.remove('dark-mode');
+        document.querySelector('.navbar').classList.add('navbar-light');
+        document.querySelector('.navbar').classList.add('bg-light');
+        document.querySelector('.navbar').classList.remove('navbar-dark');
+        document.querySelectorAll('.card').forEach(card => card.classList.remove('dark-mode'));
+        document.querySelectorAll('.btn').forEach(btn => btn.classList.remove('dark-mode'));
+        document.querySelectorAll('.form-control').forEach(input => input.classList.remove('dark-mode'));
+        document.querySelectorAll('.carousel-item').forEach(item => item.classList.remove('dark-mode'));
+        document.querySelector('footer').classList.remove('dark-mode');
+        localStorage.setItem('dark-mode', 'disabled');
+    }
+});
